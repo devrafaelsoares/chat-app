f1cbce75509e2bef59e210084a0bcfd17b53f171
devrafaelsoares <rafael.soares.developer@gmail.com>
2024-01-28 15:51:14 -0300

    feat: project init

---
+++ src/main/java/br/devrafaelsoares/chatapplication/ChatApplication.java
@@ -0,0 +1,13 @@
+package br.devrafaelsoares.chatapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+@SpringBootApplication
+@EnableMongoRepositories
+public class ChatApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ChatApplication.class, args);
+	}
+}
