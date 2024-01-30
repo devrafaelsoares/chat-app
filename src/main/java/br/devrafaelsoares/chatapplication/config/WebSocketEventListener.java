abc9050314d902e0106c7f792fb5ac2c3afadba2
devrafaelsoares <rafael.soares.developer@gmail.com>
2024-01-30 13:02:24 -0300

    feat: implementing websocket configuration

---
+++ src/main/java/br/devrafaelsoares/chatapplication/config/WebSocketEventListener.java
@@ -0,0 +1,49 @@
+package br.devrafaelsoares.chatapplication.config;
+
+import br.devrafaelsoares.chatapplication.chat_message.ChatMessage;
+import br.devrafaelsoares.chatapplication.chat_message.MessageType;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cglib.core.Local;
+import org.springframework.context.event.EventListener;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.messaging.SessionDisconnectEvent;
+
+import java.sql.Date;
+import java.sql.Timestamp;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class WebSocketEventListener {
+
+    @Autowired
+    private SimpMessageSendingOperations messageSendingOperations;
+    @EventListener
+    public void webSocketDisconnectListener(
+            SessionDisconnectEvent event
+    ) {
+        StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
+        String username = (String) headerAccessor.getSessionAttributes().get("username");
+
+        if(username != null) {
+
+            log.info("User disconnected: {}", username);
+
+            ChatMessage chatMessage = ChatMessage
+                    .builder()
+                    .sender(username)
+                    .type(MessageType.LEAVE)
+                    .timestamp(Date.from(Instant.now().atZone(ZoneId.systemDefault()).toInstant()))
+                    .build();
+
+            messageSendingOperations.convertAndSend("/topic/public", chatMessage);
+        }
+    }
+}
